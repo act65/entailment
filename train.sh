ae8a1922e03b11491a8ebae0a33346896a59da72
act65 <te1farac@gmail.com>
2018-06-25 08:35:15 +1200

    .

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 BUCKET_NAME="gs://entails"
-JOB_NAME="entail_10"
+JOB_NAME="pwn256"
 gcloud ml-engine jobs submit training "${JOB_NAME}" \
 --stream-logs \
 --module-name entailment.train_possible_worlds_net \
@@ -7,8 +7,9 @@ gcloud ml-engine jobs submit training "${JOB_NAME}" \
 --staging-bucket "${BUCKET_NAME}" \
 --region "us-central1" \
 --runtime-version=1.8 \
---scale-tier="basic-tpu" \
+--scale-tier="basic-gpu" \
 -- \
 --batch_size=64 \
 --logdir=$BUCKET_NAME/$JOB_NAME/logs/0 \
---datadir=$BUCKET_NAME/entailment/logical_entailment_dataset/data
+--datadir=$BUCKET_NAME/entailment/logical_entailment_dataset/data \
+--n_world=256
